42efa76744dca7fb01c65776c8538116b2506618
Athirai S <58971257+athirai-s@users.noreply.github.com>
2024-04-04 00:43:37 +0530

    Delete StripeWebApp directory

--- StripeWebApp/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace StripeWebApp.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- StripeWebApp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StripeWebApp.WebApiApplication" Language="C#" %>
